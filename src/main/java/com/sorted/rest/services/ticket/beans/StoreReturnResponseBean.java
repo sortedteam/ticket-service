b0542cc38311a1f992f488187843b53f6ab508aa
eam19-SORTED <aakash.maurya@sorted.team>
2023-06-27 23:18:03 +0530

    minor fix

--- src/main/java/com/sorted/rest/services/ticket/beans/StoreReturnResponseBean.java
+++ src/main/java/com/sorted/rest/services/ticket/beans/StoreReturnResponseBean.java
@@ -11,23 +11,11 @@ public class StoreReturnResponseBean implements Serializable {
 
 	private static final long serialVersionUID = -6385345671727969023L;
 
-	private int id;
-
 	private int storeId;
-
-	private int whId;
 
 	private Date returnDate;
 
 	private String status;
-
-	private String createdBy;
-
-	private Date createdOn;
-
-	private String modifiedBy;
-
-	private Date modifiedOn;
 
 	private String orderId;
 
