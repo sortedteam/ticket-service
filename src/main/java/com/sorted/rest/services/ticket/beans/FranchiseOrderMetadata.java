b469a3d2be1ad47d24e409f02bf9f929c2660e51
eam19-SORTED <121690600+eam19-SORTED@users.noreply.github.com>
2023-09-25 16:53:09 +0530

    proof of delivery (#35)
    * proof of delivery
    
    * addressed review comments

---
+++ src/main/java/com/sorted/rest/services/ticket/beans/FranchiseOrderMetadata.java
@@ -0,0 +1,15 @@
+package com.sorted.rest.services.ticket.beans;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+public class FranchiseOrderMetadata implements Serializable {
+
+	private static final long serialVersionUID = 7649495743589360269L;
+
+	private List<FranchiseOrderDeliveryBean> deliveryDetails;
+}
